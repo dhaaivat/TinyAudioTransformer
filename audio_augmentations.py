c05040bd1dc94ad28db23f2c0b161b39eafed8d9
Dhaaivat Patil <149309296+dhaaivat@users.noreply.github.com>
2025-07-28 19:29:28 +0530

    Update audio_augmentations.py

--- audio_augmentations.py
+++ audio_augmentations.py
@@ -1,12 +1,11 @@
-from torch_audiomentations import Compose, Gain, PolarityInversion, Shift, AddBackgroundNoise
+from torch_audiomentations import Compose, Gain, PolarityInversion, Shift
 
-def get_waveform_augmentations(sample_rate: int = 16000, noise_dir: str = None):
+def get_waveform_augmentations(sample_rate: int = 16000):
     augment = Compose(
         transforms=[
             PolarityInversion(p=0.5),
             Gain(min_gain_in_db=-6.0, max_gain_in_db=6.0, p=0.5),
             Shift(min_shift=-0.5, max_shift=0.5, p=0.5),
-            AddBackgroundNoise(background_paths=noise_dir, p=0.5, sample_rate=sample_rate),
         ]
     )
     return augment
