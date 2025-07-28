dc14cf1fb789dd43e73ed3e7a86dd4ce8448eb4c
Dhaaivat Patil <149309296+dhaaivat@users.noreply.github.com>
2025-07-28 18:01:35 +0530

    Update audio_augmentations.py

--- audio_augmentations.py
+++ audio_augmentations.py
@@ -6,14 +6,7 @@ def get_waveform_augmentations(sample_rate: int = 16000):
         transforms=[
             PolarityInversion(p=0.5),
             Gain(min_gain_in_db=-6.0, max_gain_in_db=6.0, p=0.5),
-            Shift(min_shift=-0.1, max_shift=0.1, p=0.5),  # time shift
-            AddBackgroundNoise(
-                background_paths=None,  # or path to your background noise dir
-                min_snr_in_db=5,
-                max_snr_in_db=20,
-                p=0.5
-            ),
-            # ApplyImpulseResponse(impulse_response_paths=..., p=0.3), # Optional
+            Shift(min_shift=-0.1, max_shift=0.1, p=0.5) # time shift
         ],
         p=0.5,
         sample_rate=sample_rate,
