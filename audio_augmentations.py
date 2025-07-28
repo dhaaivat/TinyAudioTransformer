aa841ee40cd4034591f1947913d59bdc3e04b33b
Dhaaivat Patil <149309296+dhaaivat@users.noreply.github.com>
2025-07-28 18:09:12 +0530

    Update audio_augmentations.py

--- audio_augmentations.py
+++ audio_augmentations.py
@@ -1,15 +1,13 @@
-from torch_audiomentations import Compose, AddBackgroundNoise, Gain, PolarityInversion, Shift, ApplyImpulseResponse
-import torch
+from torch_audiomentations import Compose, PolarityInversion, Gain, Shift, AddBackgroundNoise
 
-def get_waveform_augmentations(sample_rate: int = 16000):
+def get_waveform_augmentations(sample_rate: int = 16000, noise_dir: str = None):
     augment = Compose(
         transforms=[
             PolarityInversion(p=0.5),
             Gain(min_gain_in_db=-6.0, max_gain_in_db=6.0, p=0.5),
-            Shift(min_shift=-0.1, max_shift=0.1, p=0.5) # time shift
+            Shift(min_shift=-0.5, max_shift=0.5, p=0.5),
+            AddBackgroundNoise(sounds_path=noise_dir, p=0.5, sample_rate=sample_rate),
         ],
-        p=0.5,
-        sample_rate=sample_rate,
-        # device="cuda" will automatically use GPU if available
+        output_type="tensor"  # NEW REQUIRED ARGUMENT
     )
     return augment
